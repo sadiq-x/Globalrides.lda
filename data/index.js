ad42dc2666e1d393e27238721adf34c2c66ff892
sadiq-x <rapaguel43@gmail.com>
2022-08-04 15:53:11 +0100

    Server and Database are done. All good!

---
+++ data/index.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+const URI = 'mongodb+srv://sadiq:Rafaelalves_43@cluster0.tpn4u.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(URI)
+const dbname = 'Yepkit-Build';
+
+let clientconnect;
+
+export async function database(){
+    return await client.connect().then(()=>{
+        clientconnect = client.db(dbname)
+        return true
+    })
+}
+
+export function getClient(){
+    if(clientconnect){
+        return clientconnect
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,47 @@
+import expres from 'express';
+const app = expres();
+const PORT = process.env.PORT_SRV || 9000;
+let state;
+
+import { database } from '../data/index.js';
+
+import fs from 'fs';
+const templateHTML = fs.readFileSync('./src/index.html', 'utf-8');
+app.use(expres.static('./src'));
+app.use(expres.json())
+
+async function main() {
+    state = await database()
+    if (state) {
+        server()
+    } else {
+        setTimeout(() => { main() }, 5000, console.log('Trying connect to server...'))
+    }
+}
+
+function server() {
+    if (state) {
+        app.listen(PORT, () => {
+            console.log(`Server online at port ${PORT}`)
+        })
+    }
+}
+
+//Endpoints
+
+app.use('/analytics', (req, res) => {
+    let view = templateHTML
+    res.send(view)
+})
+
+
+
+
+
+
+
+
+
+
+
+main()
